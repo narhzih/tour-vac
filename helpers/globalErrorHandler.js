d639dd081749f8ab180d0dcd69f46a041d2dc46f
Symfony <omosekejiolawale@gmail.com>
2021-04-18 16:59:35 +0100

    Separated Prod error from Dev error

--- helpers/globalErrorHandler.js
+++ helpers/globalErrorHandler.js
@@ -1,10 +1,32 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.message = err.message || 'error';
+const sendProdError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      message: 'Something went very wrong',
+    });
+  }
+};
 
+const sendDevError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
+    stack: err.stack,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDevError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProdError(err, res);
+  }
 };
