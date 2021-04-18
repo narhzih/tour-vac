734a091261a1c24af138772cfaa54be50af70319
Symfony <omosekejiolawale@gmail.com>
2021-04-18 01:51:31 +0100

    Removed file reading functionalites from controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const fs = require('fs');
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
+
+const Tour = require('./../models/tourModel');
 
 // Controller Middlewares
-exports.checkID = (req, res, next) => {
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    res.status(400).json({
-      status: 'error',
-      message: 'Invalid tour ID',
-    });
-  }
-
-  next();
-};
+// exports.checkID = (req, res, next) => {
+//   const id = req.params.id * 1;
+//   if (id > tours.length) {
+//     res.status(400).json({
+//       status: 'error',
+//       message: 'Invalid tour ID',
+//     });
+//   }
+//
+//   next();
+// };
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -27,25 +29,41 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'An error occurred',
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+exports.getTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tour.findById(id);
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'An error occurred',
+      error: err,
+    });
+  }
 };
 
 exports.createNewTour = (req, res) => {
