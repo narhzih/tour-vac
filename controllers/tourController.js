b3538ba7074cecfb0a85310474223a71ee08d381
Symfony <omosekejiolawale@gmail.com>
2021-04-18 10:40:44 +0100

    Added sorting and field limiting to get-all

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,7 +39,25 @@ exports.getAllTours = async (req, res) => {
     // Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const tours = await Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      let sortQuery = req.query.sort.split(',').join(' ');
+      query = query.sort(sortQuery);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    const tours = await query;
     res.status(201).json({
       status: 'success',
       data: {
